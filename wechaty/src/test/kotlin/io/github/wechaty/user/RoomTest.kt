431b2683220658caa12d5ae1797720b51fc692c7
renxiaoya <renxiaoya@shuidihuzhu.com>
2020-05-29 19:09:19 +0800

    create unit test init

---
+++ wechaty/src/test/kotlin/io/github/wechaty/user/RoomTest.kt
@@ -0,0 +1,58 @@
+package io.github.wechaty.user
+
+import io.github.wechaty.Wechaty
+import io.github.wechaty.util.MockitoHelper
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+
+/**
+ * @author renxiaoya
+ * @date 2020-05-29
+ */
+class RoomTest {
+
+    lateinit var mockWechaty: Wechaty
+
+    lateinit var room: Room
+
+    @Before
+    fun setUp() {
+        //TODO(create a mock puppet for unit test)
+        //these can not fill the inner field in wechaty.To resolve it,I may define a mock puppet for unit test
+        mockWechaty = Mockito.mock(Wechaty::class.java)
+        room = Mockito.mock(Room::class.java)
+
+    }
+
+    @After
+    fun tearDown() {
+
+    }
+
+    @Test
+    // ignore temporary because it doesn't work for now
+    @Ignore
+    fun say() {
+        `when`(room.alias(MockitoHelper.anyObject())).thenReturn("test-alias")
+        val msgId = "test_msgId"
+        `when`(mockWechaty.getPuppet().messageSendText(ArgumentMatchers.anyString()
+            , ArgumentMatchers.anyString()
+            , ArgumentMatchers.anyList()).get()).thenReturn(msgId)
+        val msg = Mockito.mock(Message::class.java)
+        `when`(mockWechaty.messageManager.load(msgId)).thenReturn(msg)
+
+        val text = "test-text"
+        val contact1 = Contact(mockWechaty, "contact1")
+        val contact2 = Contact(mockWechaty, "contact2")
+        val contact3 = Contact(mockWechaty, "contact3")
+
+        verify(room.say(text, listOf(contact1, contact2, contact3)))
+
+    }
+}
---
+++ wechaty/src/test/kotlin/io/github/wechaty/util/MockitoHelper.kt
@@ -0,0 +1,19 @@
+package io.github.wechaty.util
+
+import org.mockito.Mockito
+
+/**
+ * A helper for Mockito
+ *
+ * @author renxiaoya
+ * @date 2020-05-29
+ **/
+object MockitoHelper {
+    fun <T> anyObject(): T {
+        Mockito.any<T>()
+        return uninitialized()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    fun <T> uninitialized(): T = null as T
+}
