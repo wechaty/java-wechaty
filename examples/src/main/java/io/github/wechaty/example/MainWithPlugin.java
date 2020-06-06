9405ff862d3026b54b075d009e7d3e8c5c6093f6
Zhengxin <diaozxin@163.com>
2020-06-06 22:30:42 +0800

    update 0.1.3
    support plugins!

--- examples/src/main/java/io/github/wechaty/example/MainWithPlugin.java
+++ examples/src/main/java/io/github/wechaty/example/MainWithPlugin.java
@@ -1,14 +1,14 @@
 package io.github.wechaty.example;
 
 import io.github.wechaty.Wechaty;
-import io.github.wechaty.plugins.WecahtyPlugins;
+import io.github.wechaty.plugins.WechatyPlugins;
 
 public class MainWithPlugin {
 
     public static void main(String[] args) {
 
         Wechaty bot = Wechaty.instance("your-token")
-            .use(WecahtyPlugins.ScanPlugin(), WecahtyPlugins.DingDongPlugin(null))
+            .use(WechatyPlugins.ScanPlugin(), WechatyPlugins.DingDongPlugin(null))
             .start(true);
 
     }
