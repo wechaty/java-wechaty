8312cb2e23f6221fc1e69eb410bb69318e7b0948
cunkoulaocai <1184016190@qq.com>
2020-07-07 13:28:25 +0800

    add MemoryCard.kt method like load save, delete

--- wechaty-puppet/src/main/kotlin/io/github/wechaty/memorycard/MemoryCard.kt
+++ wechaty-puppet/src/main/kotlin/io/github/wechaty/memorycard/MemoryCard.kt
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory
 import java.lang.Exception
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Future
+import kotlin.math.abs
 
 const val NAMESPACE_MULTIPLEX_SEPRATOR = "\r"
 const val NAMESPACE_KEY_SEPRATOR       = "\n"
@@ -22,7 +23,8 @@ class MemoryCard {
 
     private var options:MemoryCardOptions
 
-    constructor(name: String?=null,options:MemoryCardOptions? = null){
+    constructor(name: String? = null,options:MemoryCardOptions? = null){
+        log.info("MemoryCard, constructor(%s)", options?.let { JsonUtils.write(it) })
 
         val _optiones:MemoryCardOptions = options ?: MemoryCardOptions()
 
@@ -37,32 +39,77 @@ class MemoryCard {
         (_optiones.multiplex != null).let {
             this.parent = _optiones.multiplex!!.parent
             this.payload = this.parent!!.payload
+
             this.multiplexNameList.addAll(parent!!.multiplexNameList)
             this.multiplexNameList.add(_optiones.multiplex!!.name)
+
             this.storage = null
         }
 
         (_optiones.multiplex == null).let {
             this.payload = null
             this.multiplexNameList.clear()
+            this.storage = getStore()
         }
     }
 
-    private fun getStore():StorageBackend?{
-        log.debug("getStorage() for storage type: %s'",this.options)
+    private fun getStore(): StorageBackend?{
 
+        log.debug("getStorage() for storage type: %s",
+            if (this.options != null && this.options.storageOptions != null && this.options.storageOptions!!.type != null)
+                this.options
+            else
+                "N/A"
+        )
+
+        if (this.options == null) {
+            return null
+        }
         return StorageBackend.getStorage(
             this.options.name!!,
             this.options.storageOptions
         )
     }
 
-    fun load() {
-        this.payload = this.storage!!.load()
+    fun load(): Future<Void> {
+        log.info("MemoryCard, load() from storage: %s", this.storage ?: "N/A")
+        if (this.isMultiplex()) {
+            log.info("MemoryCard, load() should not be called on a multiplex MemoryCard. NOOP")
+            return CompletableFuture.completedFuture(null)
+        }
+        if (this.payload == null) {
+            throw Exception("memory had already loaded before.")
+        }
+
+        if (this.storage != null) {
+            this.payload = this.storage!!.load()
+        }
+        else {
+            log.info("MemoryCard, load() no storage")
+            this.payload = MemoryCardPayload()
+        }
+        return CompletableFuture.completedFuture(null)
     }
 
-    fun save() {
+    fun save(): Future<Void> {
+        if (this.isMultiplex()) {
+            if (this.parent == null) {
+                throw Exception("multiplex memory no parent")
+            }
+        }
+        this.parent!!.save()
+
+        log.info("MemoryCard, <%s>%s save() to %s",this.name ?: "", this.multiplexPath(), this.storage ?: "N/A")
+        if (this.payload == null) {
+            throw Exception("no payload, please call load() first.")
+        }
+
+        if (this.storage == null) {
+            log.info("MemoryCard, save() no storage, NOOP")
+        }
+
         this.storage!!.save(this.payload!!)
+        return CompletableFuture.completedFuture(null)
     }
 
     fun destory(): Future<Void> {
@@ -138,6 +185,86 @@ class MemoryCard {
             return name
         }
     }
+
+    fun <T : Any> get(name: String): CompletableFuture<Any?>? {
+        log.info("MemoryCard, <%s> get(%s)", this.multiplexPath(), name)
+        if (this.payload == null) {
+            throw Exception("no payload, please call load() first.")
+        }
+
+        val key = this.resolveKey(name)
+        return CompletableFuture.supplyAsync {
+            this.payload!!.map.get(key)
+        }
+    }
+
+    fun <T : Any> set(name: String, data: T): Future<Void> {
+        log.info("MemoryCard, <%s> set(%s, %s)", this.multiplexPath(), name, data)
+
+        if (this.payload == null) {
+            throw Exception("no payload, please call load() first.")
+        }
+
+        val key = this.resolveKey(name)
+        this.payload!!.map[key] = data as Any
+        return CompletableFuture.completedFuture(null)
+    }
+
+    fun clear(): Future<Void> {
+        log.info("MemoryCard, <%s> clear()", this.multiplexPath())
+        if (this.payload == null) {
+            throw Exception("no payload, please call load() first.")
+        }
+        if (this.isMultiplex()) {
+            val keys = this.payload!!.map.keys
+            for (key in keys) {
+                this.payload!!.map.remove(key)
+            }
+        }
+        else {
+            this.payload = MemoryCardPayload()
+        }
+        return CompletableFuture.completedFuture(null)
+    }
+
+    fun delete(name: String): Future<Void> {
+
+        log.info("MemoryCard, <%s> delete(%s)", this.multiplexPath(), name)
+        if (this.payload == null) {
+            throw Exception("no payload, please call load() first.")
+        }
+
+        val key = this.resolveKey(name)
+        this.payload!!.map.remove(key)
+        return CompletableFuture.completedFuture(null)
+    }
+
+    fun has(key: String): Future<Boolean> {
+        log.info("MemoryCard, <%s> has (%s)", this.multiplexPath(), key)
+        if (this.payload == null) {
+            throw Exception("no payload, please call load() first.")
+        }
+        val absoluteKey = this.resolveKey(key)
+        return CompletableFuture.supplyAsync {
+            this.payload!!.map.containsKey(absoluteKey)
+        }
+    }
+
+
+    override fun toString(): String {
+        var mpString = ""
+        if (this.multiplexNameList.size > 0) {
+            mpString = this.multiplexNameList
+                .map { mpName -> "multiplex(${mpName})" }
+                .joinToString("")
+        }
+        var name = ""
+        if (this.options != null && this.options.name != null) {
+            name = this.options.name.toString()
+        }
+
+        return "MemoryCard<${name}>${mpString}"
+    }
     companion object{
         private val log = LoggerFactory.getLogger(MemoryCard::class.java)
         val VERSION = "0.0.0"
@@ -167,7 +294,7 @@ class MemoryCard {
 }
 
 class MemoryCardPayload{
-    val map: Map<String,Any> = HashMap()
+    val map = mutableMapOf<String, Any>()
 }
 
 data class Multiplex(
