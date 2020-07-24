35efab6a57250faafe2327adc2f2182b1ed21f12
cunkoulaocai <1184016190@qq.com>
2020-07-24 22:26:57 +0800

    1. 添加获取qrcode,name,signture方法

--- wechaty/src/main/kotlin/io/github/wechaty/user/ContactSelf.kt
+++ wechaty/src/main/kotlin/io/github/wechaty/user/ContactSelf.kt
@@ -2,6 +2,8 @@ package io.github.wechaty.user
 
 import io.github.wechaty.Wechaty
 import io.github.wechaty.filebox.FileBox
+import io.github.wechaty.utils.QrcodeUtils.Companion.guardQrCodeValue
+import org.slf4j.LoggerFactory
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Future
 
@@ -17,6 +19,64 @@ class ContactSelf(wechaty: Wechaty,id: String) : Contact(wechaty,id){
             null
         }
 
+    }
+
+    fun qrcode(): Future<String> {
+        log.info("Contact, qrcode()")
+
+        val puppetId: String = try {
+            this.puppet.selfId().toString()
+        }
+        catch (e: Exception) {
+            throw Exception("Can not get qrcode, user might be either not logged in or already logged out")
+        }
+
+        if (this.id !== puppetId) {
+            throw Exception("only can get qrcode for the login userself")
+        }
+
+        return CompletableFuture.supplyAsync {
+            val qrcodeValue = this.puppet.contactSelfQRCode().get()
+            guardQrCodeValue(qrcodeValue)
+        }
+    }
+
+    fun name(name: String?): String? {
+        if (name == null) {
+            return super.name()
+        }
+        val puppetId = try {
+            this.puppet.selfId()
+        }
+        catch (e: Exception) {
+            throw Exception("Can not get qrcode, user might be either not logged in or already logged out")
+        }
+        if (this.id !== puppetId) {
+            throw Exception("only can get qrcode for the login userself")
+        }
+        this.puppet.contactSelfName(name)
+        return null
+    }
+
+    fun signature (signature: String): Future<Void> {
+        log.debug("ContactSelf, signature()")
+
+        val puppetId = try {
+            this.puppet.selfId()
+        }
+        catch (e: Exception) {
+            throw Exception("Can not get qrcode, user might be either not logged in or already logged out")
+        }
+
+        if (this.id !== puppetId) {
+            throw Exception("only can get qrcode for the login userself")
+        }
+
+        return this.puppet.contactSelfSignature(signature)
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(ContactSelf::class.java)
     }
 
 }
